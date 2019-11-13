054c8f4b69fc8801281a8ab6de76fe2e0d3043b6
Wowsaruss <russell.hayes.dev@gmail.com>
2019-11-13 14:07:47 -0700

    migrations

---
+++ migrations/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/Wowsaruss/financial-back-go/pkg/config"
+	_ "github.com/lib/pq"
+)
+
+// Transaction ...
+type Transaction struct {
+	Details           string
+	PostingDate       time.Time
+	Description       string
+	Amount            float64
+	PmtType           string
+	Balance           float64
+	CheckOrSlipNumber string
+	Type              string
+	Monthly           bool
+}
+
+func stringToBool(m string) bool {
+	if m == "TRUE" {
+		return true
+	}
+	return false
+}
+
+func main() {
+	arg := os.Args[1]
+	cfg := config.NewConfig()
+
+	csvFile, err := os.Open(arg)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer csvFile.Close()
+
+	reader := csv.NewReader(csvFile)
+	reader.FieldsPerRecord = -1
+
+	csvData, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var tran Transaction
+	var transactions []Transaction
+
+	withoutHeader := csvData[1:]
+	for _, each := range withoutHeader {
+		t, _ := time.Parse("01/02/06", each[1])
+		if err != nil {
+			fmt.Println(err)
+			panic(err)
+		}
+
+		amount, err := strconv.ParseFloat(each[3], 32)
+		if err != nil {
+			fmt.Println(err)
+			panic(err)
+		}
+
+		balance, err := strconv.ParseFloat(each[5], 32)
+		if err != nil {
+			fmt.Println(err)
+			panic(err)
+		}
+
+		tran.Details = each[0]
+		tran.PostingDate = t
+		tran.Description = each[2]
+		tran.Amount = amount
+		tran.PmtType = each[4]
+		tran.Balance = balance
+		tran.CheckOrSlipNumber = each[6]
+		tran.Type = each[7]
+		tran.Monthly = stringToBool(each[8])
+
+		transactions = append(transactions, tran)
+	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer db.Close()
+
+	count := 0
+	for _, item := range transactions {
+		sqlStatement := `INSERT INTO transactions (date, description, amount, account_balance, type, payment_type, monthly)
+		VALUES ($1, $2, $3, $4, $5, $6, $7)`
+
+		_, err = db.Exec(sqlStatement, item.PostingDate, item.Description, item.Amount, item.Balance, item.Type, item.PmtType, item.Monthly)
+		if err != nil {
+			panic(err)
+		}
+		count++
+	}
+	fmt.Printf("%v transactions inserted to db.", count)
+}
